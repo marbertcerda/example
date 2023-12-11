75c343d14a44e4e7f851b81f8cf9839b0a0ef76c
cerda_marbert <zedchixx23@gmail.com>
2023-12-11 09:22:00 +0800

    commit

--- app.js
+++ app.js
@@ -26,3 +26,21 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+// CREATE DATABASE todo_list_db;
+// USE todo_list_db;
+
+// CREATE TABLE tasks (
+//   id INT AUTO_INCREMENT PRIMARY KEY,
+//   task VARCHAR(255) NOT NULL,
+//   completed BOOLEAN DEFAULT false
+// );
+
+// authentication
+// users
+// id
+// username
+// password
+// address
+// age
